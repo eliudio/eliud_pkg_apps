96ed189aa800dd4c9c7c890958a926f6b94759ac
Eliud Berlin <info@eliud.io>
2020-12-29 22:38:07 +0100

    next step in workflow

--- lib/apps/minkey_app/workflow/workflow_setup.dart
+++ lib/apps/minkey_app/workflow/workflow_setup.dart
@@ -5,7 +5,6 @@ import 'package:eliud_pkg_workflow/model/abstract_repository_singleton.dart';
 import 'package:eliud_pkg_workflow/model/workflow_model.dart';
 import 'package:eliud_pkg_workflow/model/workflow_task_model.dart';
 import 'package:eliud_pkg_workflow/tools/action/workflow_action_model.dart';
-import 'package:eliud_pkg_workflow/tools/task/task_model.dart';
 import 'package:eliud_pkg_membership/membership_package.dart';
 
 class WorkflowSetup {
@@ -15,31 +14,51 @@ class WorkflowSetup {
 
   static WorkflowModel _workflowModel() {
     return WorkflowModel(
-      documentID: "manual_paid_membership",
-      name: "Manual Paid_membership",
-      workflowTask: [
-        WorkflowTaskModel(
-          documentID: "workflow_task_payment_manual",
-          task: FixedAmountPayModel(paymentType: PaymentType.Manual, ccy: "GBP", amount: 20),
-        ),
-        WorkflowTaskModel(
-          documentID: "workflow_task_payment_stripe",
-          task: FixedAmountPayModel(paymentType: PaymentType.Stripe, ccy: "GBP", amount: 20),
-        ),
-      ]
-    );
+        documentID: "manual_paid_membership",
+        name: "Manual Membership (paid)",
+        workflowTask: [
+          WorkflowTaskModel(
+            seqNumber: 1,
+            documentID: "workflow_task_payment_manual",
+            responsible: WorkflowTaskResponsible.CurrentMember,
+            task: FixedAmountPayModel(
+                paymentType: ManualPayTypeModel(
+                    payTo: "Mr Minkey",
+                    country: "United Kingdom",
+                    bankIdentifierCode: "Bank ID Code",
+                    payeeIBAN: "IBAN 543232187632167",
+                    bankName: "Bank Of America"),
+                ccy: "GBP",
+                amount: 20),
+          ),
+          WorkflowTaskModel(
+            seqNumber: 2,
+            documentID: "workflow_task_payment_stripe1",
+            responsible: WorkflowTaskResponsible.CurrentMember,
+            task: FixedAmountPayModel(
+                paymentType: CreditCardPayTypeModel(), ccy: "GBP", amount: 20),
+          ),
+          WorkflowTaskModel(
+            seqNumber: 3,
+            documentID: "workflow_task_payment_stripe2",
+            responsible: WorkflowTaskResponsible.Owner,
+            task: FixedAmountPayModel(
+                paymentType: CreditCardPayTypeModel(), ccy: "GBP", amount: 20),
+          ),
+        ]);
   }
 
   Future<WorkflowModel> _setupWorkflow() async {
-    return await AbstractRepositorySingleton.singleton.workflowRepository(installApp.appId).add(_workflowModel());
+    return await AbstractRepositorySingleton.singleton
+        .workflowRepository(installApp.appId)
+        .add(_workflowModel());
   }
 
-  static WorkflowAction requestMembershipAction(String appId) => WorkflowAction(
-      appId,
-      readCondition: ReadCondition.PackageDecides,
-      packageCondition: MembershipPackage.MEMBER_HAS_NO_MEMBERSHIP_YET,
-      workflow: _workflowModel()
-  );
+  static WorkflowAction requestMembershipAction(String appId) =>
+      WorkflowAction(appId,
+          readCondition: ReadCondition.PackageDecides,
+          packageCondition: MembershipPackage.MEMBER_HAS_NO_MEMBERSHIP_YET,
+          workflow: _workflowModel());
 
   void run() async {
     await _setupWorkflow();
