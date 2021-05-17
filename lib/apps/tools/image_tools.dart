a6aeb255577d77a3b75e2e65610e72b919d9ae5f
Eliud Berlin <info@eliud.io>
2021-05-17 17:22:01 +0100

    Upload

--- lib/apps/tools/image_tools.dart
+++ lib/apps/tools/image_tools.dart
@@ -8,7 +8,7 @@ import 'package:eliud_core/model/home_menu_model.dart';
 import 'package:eliud_core/model/model_export.dart';
 import 'package:eliud_core/model/page_model.dart';
 import 'package:eliud_core/tools/random.dart';
-import 'package:eliud_core/tools/storage/firestore_helper.dart';
+import 'package:eliud_core/tools/storage/member_medium_helper.dart';
 import 'package:eliud_pkg_fundamentals/model/tutorial_component.dart';
 import 'package:eliud_pkg_fundamentals/model/tutorial_entry_model.dart';
 import 'package:eliud_pkg_fundamentals/model/tutorial_model.dart';
@@ -16,19 +16,19 @@ import 'package:eliud_pkg_feed/model/post_medium_model.dart';
 
 class ImageTools {
   static Future<MemberMediumModel> uploadPublicPhoto(String appId, MemberModel member, String assetPath) async {
-    return await UploadFile.createThumbnailUploadPhotoAsset(appId, assetPath, member.documentID!, ['PUBLIC',member.documentID!]);
+    return await MemberMediumHelper.createThumbnailUploadPhotoAsset(appId, assetPath, member.documentID!, ['PUBLIC',member.documentID!]);
   }
 
   static Future<MemberMediumModel> uploadPublicVideo(String appId, MemberModel member, String assetPath) async {
-    return await UploadFile.createThumbnailUploadVideoAsset(appId, assetPath, member.documentID!, ['PUBLIC',member.documentID!]);
+    return await MemberMediumHelper.createThumbnailUploadVideoAsset(appId, assetPath, member.documentID!, ['PUBLIC',member.documentID!]);
   }
 
   static Future<MemberMediumModel> uploadPublicPdf(String appId, MemberModel member, String assetPath, {String? documentID}) async {
-    return await UploadFile.createThumbnailUploadPdfAsset(appId, assetPath, member.documentID!, ['PUBLIC',member.documentID!], documentID: documentID);
+    return await MemberMediumHelper.createThumbnailUploadPdfAsset(appId, assetPath, member.documentID!, ['PUBLIC',member.documentID!], documentID: documentID);
   }
 
   static Future<PostMediumModel> createPostMediumModelPhoto(String appId, MemberModel member, String assetPath) async {
-    var photo = await UploadFile.createThumbnailUploadPhotoAsset(appId, assetPath, member.documentID!, ['PUBLIC',member.documentID!]);
+    var photo = await MemberMediumHelper.createThumbnailUploadPhotoAsset(appId, assetPath, member.documentID!, ['PUBLIC',member.documentID!]);
     return PostMediumModel(
       documentID: newRandomKey(),
       memberMedium: photo,
