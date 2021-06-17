c0f3084c3ba1981cf027486fe3b1c6322df38248
Eliud <58745656+eliudio@users.noreply.github.com>
2021-06-17 12:05:05 +0100

    No more warnings during run

--- lib/apps/app_base.dart
+++ lib/apps/app_base.dart
@@ -189,7 +189,7 @@ abstract class InstallApp {
     var backgroundModel = BackgroundModel(
         documentID: 'left_drawer_header_bg',
         decorationColors: decorationColorModels,
-        backgroundImageURL: logo != null ? logo!.url : null);
+        backgroundImageURL: logo != null ? logo.url : null);
     return backgroundModel;
   }
 
