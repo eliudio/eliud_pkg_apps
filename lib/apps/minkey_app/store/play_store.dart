85a7eb26dceae1436fd650993b51fc70cd059d62
Eliud <58745656+eliudio@users.noreply.github.com>
2022-02-08 16:19:05 +0000

    ?

--- lib/apps/minkey_app/store/play_store.dart
+++ lib/apps/minkey_app/store/play_store.dart
@@ -144,7 +144,6 @@ class PlayStore extends AppSection {
 
   ShadowModel _shadowModel() {
     ShadowModel shadowModel = ShadowModel(
-        comments: "Store Item shadow",
         color: EliudColors.grayTransparent,
         offsetDX: 4,
         offsetDY: 3,
