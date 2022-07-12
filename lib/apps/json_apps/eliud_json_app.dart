a184bde5bd2dcf736b4a3df53a4a8665e4c54a7d
Eliud <58745656+eliudio@users.noreply.github.com>
2022-07-12 12:37:12 +0100

    docs

---
+++ lib/apps/json_apps/eliud_json_app.dart
@@ -0,0 +1,13 @@
+import 'package:eliud_core/model/app_model.dart';
+
+import '../json_app.dart';
+
+class EliudJsonApp extends JsonApp {
+  static String ELIUD_APP_ID = "ELIUD_APP2";
+
+  static AppModel app = AppModel(documentID: ELIUD_APP_ID, ownerID: '?');
+
+  EliudJsonApp() : super(app);
+
+  String jsonLocation() => 'packages/eliud_pkg_apps/assets/json_apps/eliud.json';
+}
---
+++ lib/apps/json_apps/juuwle_json_app.dart
@@ -0,0 +1,13 @@
+import 'package:eliud_core/model/app_model.dart';
+
+import '../json_app.dart';
+
+class JuuwleJsonApp extends JsonApp {
+  static String JUUWLE_APP_ID = "JUUWLE_APP2";
+
+  static AppModel app = AppModel(documentID: JUUWLE_APP_ID, ownerID: '?');
+
+  JuuwleJsonApp() : super(app);
+
+  String jsonLocation() => 'packages/eliud_pkg_apps/assets/json_apps/juuwle.json';
+}
